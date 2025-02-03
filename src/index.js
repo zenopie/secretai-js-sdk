265696cf0fc4d3f0318f4f771246d3b481545913
Braydn Larsen <braydnl@gmail.com>
2025-02-03 07:26:32 -0800

    slimmed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // src/index.js
 import * as config from './config.js';
-import { Secret } from './secret.js';
 import { ChatSecret } from './chat-secret.js';
 import { SecretAIError, SecretAINetworkError } from './utils/errors.js';
 
-export { Secret, ChatSecret, SecretAIError, SecretAINetworkError, config };
+export { ChatSecret, SecretAIError, SecretAINetworkError, config };
