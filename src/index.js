e1b5f0a59a0c6f9ffd1c01b7e5ac6db623036e61
Braydn Larsen <braydnl@gmail.com>
2025-02-04 00:07:13 -0800

    change config export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // src/index.js
-import * as config from './config.js';
+import * as SECRET_AI_CONFIG from './config.js';
 import { ChatSecret } from './chat-secret.js';
 import { SecretAIError, SecretAINetworkError } from './utils/errors.js';
 
-export { ChatSecret, SecretAIError, SecretAINetworkError, config };
+export { ChatSecret, SecretAIError, SecretAINetworkError, SECRET_AI_CONFIG };
